9a72de2912dfd28598fd409f122cb12cc7a41cee
Philipp Kaiser <p.kaiser95@gmx.de>
2022-03-16 16:05:27 +0100

    Implemented datatable for CRUD functions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "./style/main.css";
 import router from "./router";
 import PrimeVue from "primevue/config";
 import "primevue/resources/primevue.min.css";
 import "primevue/resources/themes/saga-blue/theme.css";
 import "/node_modules/primeflex/primeflex.css";
+import "primeicons/primeicons.css";
 
 Vue.use(PrimeVue);
 
--- src/views/VehicleTypes.vue
+++ src/views/VehicleTypes.vue
@@ -1,5 +1,206 @@
 <template>
-  <div class="vehicles">
-    <h1>This is an VehiclesTypes page</h1>
+  <div class="card">
+    <Toolbar class="mb-4">
+      <template #start>
+        <Button
+          label="New"
+          icon="pi pi-plus"
+          class="p-button-success mr-2"
+          @click="openNew"
+        />
+      </template>
+    </Toolbar>
+    <DataTable :value="vehicleTypes" responsiveLayout="scroll">
+      <Column field="name" header="Fahrzeugtyp"></Column>
+      <Column :styles="{ 'min-width': '8rem' }">
+        <template #body="slotProps">
+          <Button
+            icon="pi pi-pencil"
+            class="p-button-rounded p-button-success mr-2"
+            @click="editVehicleType(slotProps.data)"
+          />
+          <Button
+            icon="pi pi-trash"
+            class="p-button-rounded p-button-warning"
+            @click="confirmDeleteVehicleType(slotProps.data)"
+          />
+        </template>
+      </Column>
+    </DataTable>
+
+    <Dialog
+      :visible.sync="typeDialog"
+      :style="{ width: '450px' }"
+      header="Fahrzeugtyp"
+      :modal="true"
+      class="p-fluid"
+    >
+      <div class="field">
+        <label for="name">Name</label>
+        <InputText
+          id="name"
+          v-model.trim="vehicleType.name"
+          required="true"
+          autofocus
+          :class="{ 'p-invalid': submitted && !vehicleType.name }"
+        />
+        <small class="p-invalid" v-if="submitted && !vehicleType.name"
+          >Name is required.</small
+        >
+      </div>
+      <template #footer>
+        <Button
+          label="Cancel"
+          icon="pi pi-times"
+          class="p-button-text"
+          @click="hideDialog"
+        />
+        <Button
+          label="Save"
+          icon="pi pi-check"
+          class="p-button-text"
+          @click="saveEditedType"
+        />
+      </template>
+    </Dialog>
+
+    <Dialog
+      :visible.sync="deleteTypeDialog"
+      :styles="{ width: '450px' }"
+      header="Confirm"
+      :modal="true"
+    >
+      <div class="confirmation-content">
+        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+        <span v-if="vehicleType"
+          >Are you sure you want to delete <b>{{ vehicleType.name }}</b
+          >?</span
+        >
+      </div>
+      <template #footer>
+        <Button
+          label="No"
+          icon="pi pi-times"
+          class="p-button-text"
+          @click="deleteTypeDialog = false"
+        />
+        <Button
+          label="Yes"
+          icon="pi pi-check"
+          class="p-button-text"
+          @click="deleteType"
+        />
+      </template>
+    </Dialog>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import Dialog from "primevue/dialog";
+import InputText from "primevue/inputtext";
+import {
+  deleteVehicleType,
+  getVehicleTypes,
+  updateVehicleType,
+} from "@/lib/apiCallHelpers";
+import VehicleType from "@/types/VehicleType";
+
+interface VehicleTypesData {
+  vehicleTypes: [];
+  typeDialog: boolean;
+  deleteTypeDialog: boolean;
+  vehicleType: VehicleType;
+  submitted: boolean;
+  newVehicleType: { name: string };
+}
+
+export default Vue.extend({
+  components: { DataTable, Column, Button, Toolbar, Dialog, InputText },
+  data(): VehicleTypesData {
+    return {
+      vehicleTypes: [],
+      typeDialog: false,
+      deleteTypeDialog: false,
+      vehicleType: { name: "", id: 0 },
+      submitted: false,
+      newVehicleType: { name: "" },
+    };
+  },
+  async created() {
+    const initialVehicleTypes = await getVehicleTypes();
+    this.vehicleTypes = initialVehicleTypes;
+  },
+  methods: {
+    openNew() {
+      this.newVehicleType = { name: "" };
+      this.submitted = false;
+      this.typeDialog = true;
+    },
+    hideDialog() {
+      this.typeDialog = false;
+      this.submitted = false;
+    },
+    saveNewType() {
+      this.submitted = true;
+      this.typeDialog = false;
+    },
+    saveEditedType() {
+      this.submitted = true;
+      const updatedVehicleType = {
+        id: this.vehicleType.id,
+        name: this.vehicleType.name,
+      };
+      // Route is n ot working?
+      updateVehicleType(updatedVehicleType);
+      this.vehicleType = { name: "", id: 0 };
+      this.typeDialog = false;
+    },
+    editVehicleType(presentVehicleType: VehicleType) {
+      this.vehicleType = presentVehicleType;
+      this.typeDialog = true;
+    },
+    confirmDeleteVehicleType(presentVehicleType: VehicleType) {
+      this.vehicleType = presentVehicleType;
+      this.deleteTypeDialog = true;
+    },
+    deleteType() {
+      const updatedVehicleType = {
+        id: this.vehicleType.id,
+        name: this.vehicleType.name,
+      };
+      deleteVehicleType(updatedVehicleType);
+      this.vehicleType = { name: "", id: 0 };
+      this.deleteTypeDialog = false;
+    },
+  },
+});
+</script>
+
+<style>
+table-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.product-image {
+  width: 100px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.p-dialog .product-image {
+  width: 150px;
+  margin: 0 auto 2rem auto;
+  display: block;
+}
+.confirmation-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
